6a4a8ece4895ebaebe792f09707fc4fd5e29d143
Carlos00-pixel <carlos.mayoral00@gmail.com>
2023-03-23 10:07:57 +0100

    Commit

--- MvcCoreLinqToXML/Controllers/PeliculasController.cs
+++ MvcCoreLinqToXML/Controllers/PeliculasController.cs
@@ -18,5 +18,41 @@ namespace MvcCoreLinqToXML.Controllers
             List<Pelicula> peliculas = this.repo.GetPeliculas();
             return View(peliculas);
         }
+
+        public IActionResult EscenasPelicula(int idpelicula)
+        {
+            Pelicula peli = this.repo.FindPelicula(idpelicula);
+            ViewData["PELICULA"] = peli;
+            List<Escena> escenas = this.repo.GetEscenasPelicula(idpelicula);
+            return View(escenas);
+        }
+
+        public IActionResult DetallePeliculaEscena(int idpelicula, int? posicion)
+        {
+            //LINQ FUNCIONA EN BASE 0
+            //SQL SERVER FUNCIONA EN BASE 1
+            if (posicion == null)
+            {
+                posicion = 0;
+            }
+            int numeroEscenas = 0;
+            Escena escena = this.repo.GetEscenaPelicula(idpelicula, posicion.Value, ref numeroEscenas);
+            ViewData["DATOS"] = "Escena " + (posicion + 1) + " de " + numeroEscenas;
+            int siguiente = posicion.Value + 1;
+            if(siguiente >= numeroEscenas)
+            {
+                siguiente = 0;
+            }
+            int anterior = posicion.Value - 1;
+            if(anterior < 0)
+            {
+                anterior = numeroEscenas - 1;
+            }
+            ViewData["SIGUIENTE"] = siguiente;
+            ViewData["ANTERIOR"] = anterior;
+            Pelicula peli = this.repo.FindPelicula(idpelicula);
+            ViewData["PELICULA"] = peli;
+            return View(escena);
+        }
     }
 }
--- MvcCoreLinqToXML/Repositories/RepositoryPeliculas.cs
+++ MvcCoreLinqToXML/Repositories/RepositoryPeliculas.cs
@@ -38,5 +38,60 @@ namespace MvcCoreLinqToXML.Repositories
             }
             return peliculas;
         }
+
+        public Pelicula FindPelicula(int idpelicula)
+        {
+            string path = helper.MapPath("peliculas.xml", Folders.Documents);
+
+            XDocument document = XDocument.Load(path);
+            var consulta = from datos in document.Descendants("pelicula")
+                           where datos.Attribute("idpelicula").Value == idpelicula.ToString()
+                           select datos;
+            XElement tag = consulta.FirstOrDefault();
+            Pelicula pelicula = new Pelicula();
+
+            pelicula.IdPelicula = int.Parse(tag.Attribute("idpelicula").Value);
+            pelicula.Titulo = tag.Element("titulo").Value;
+            pelicula.TituloOriginal = tag.Element("titulooriginal").Value;
+            pelicula.Descripcion = tag.Element("descripcion").Value;
+            pelicula.Poster = tag.Element("poster").Value;
+            return pelicula;
+        }
+
+        public List<Escena> GetEscenasPelicula(int idpelicula)
+        {
+            string path = this.helper.MapPath("escenaspeliculas.xml", Folders.Documents);
+            XDocument document = XDocument.Load(path);
+            var consulta = from datos in document.Descendants("escena")
+                           where datos.Attribute("idpelicula").Value ==
+                           idpelicula.ToString()
+                           select datos;
+            List<Escena> escenas = new List<Escena>();
+            foreach(XElement tag in consulta)
+            {
+                Escena escena = new Escena();
+                escena.IdPelicula = int.Parse(tag.Attribute("idpelicula").Value);
+                escena.TituloEscena = tag.Element("tituloescena").Value;
+                escena.Descripcion = tag.Element("descripcion").Value;
+                escena.Imagen = tag.Element("imagen").Value;
+
+                escenas.Add(escena);
+            }
+            return escenas;
+        }
+
+        public Escena GetEscenaPelicula(int idpelicula, int posicion, ref int numeroEscenas)
+        {
+            string path =
+                this.helper.MapPath("escenaspeliculas.xml", Folders.Documents);
+            XDocument document = XDocument.Load(path);
+            //VOY A RECUPERAR LA COLECCION DE ESCENAS DE UNA PELICULA
+            //PARA ELLO, UTILIZAMOS EL METODO ANTERIOR
+            List<Escena> escenas = this.GetEscenasPelicula(idpelicula);
+            numeroEscenas = escenas.Count;
+            //VAMOS A PAGINAR DE UNO EN UNO
+            Escena escena = escenas.Skip(posicion).Take(1).FirstOrDefault();
+            return escena;
+        }
     }
 }
